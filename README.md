99a152892b5a6cab4a754801ea9757ccef226cf7
mstxq17 <154380808@qq.com>
2022-07-20 23:30:35 +0800

    Init Program

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # MoreIP
  一款基于Python的面向MacOS用户用于查询IP信息的日常渗透小工具。
+
+# Program
---
+++ main.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env
+# -*- coding:utf-8 -*-
+
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()

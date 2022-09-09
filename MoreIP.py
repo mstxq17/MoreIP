68eb21c4564bda889c1e296e8a79d84c9dc61e93
mstxq17 <154380808@qq.com>
2022-09-09 15:49:06 +0800

    Program initializing

---
+++ MoreIP.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# @Time : 2022/9/9 3:13 PM
+# @Author : xq17
+# @FileName : MoreIP.py.py
+# @Software: PyCharm
+
+def main():
+    print("ok")
+
+
+if __name__ == '__main__':
+    main()
